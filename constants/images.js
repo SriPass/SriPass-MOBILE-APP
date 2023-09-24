a2c162bf6e468833a47c508be35c6f6922219354
Imesh Pasinda <imeshpasinda@gmail.com>
2023-09-24 11:16:13 +0530

    Initial Commit

---
+++ constants/images.js
@@ -0,0 +1,18 @@
+const sripassLogo = require("../assets/images/sripass-logo.png");
+const banner = require("../assets/images/banner.png");
+const subBanner = require("../assets/images/sub-banner.png");
+const focus = require("../assets/images/focus.png");
+
+// Dummy
+const usFlag = require("../assets/images/us-flag.jpg");
+
+
+export default {
+    sripassLogo,
+    banner,
+    subBanner,
+    focus,
+
+    // Dummy
+    usFlag
+}
